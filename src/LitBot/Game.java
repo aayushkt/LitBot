e581f40a6e9b5ff27549dcf699ad00510b9436b8
Aayush Singh <aayushkt@gmail.com>
2023-06-05 13:46:33 -0400

    hardcoded cards + started turn functionality

--- src/LitBot/Game.java
+++ src/LitBot/Game.java
@@ -1,8 +1,17 @@
 package LitBot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 public class Game {
 
-    static CardHolder[][] cards = new CardHolder[9][6];
+    static final String[] cardStrings = {"2S", "3S", "4S", "5S", "6S", "7S", "2H", "3H", "4H", "5H", "6H", "7H", "2D", "3D", "4D", "5D", "6D", "7D", "2C", "3C", "4C", "5C", "6C", "7C", "9S", "TS", "JS", "QS", "KS", "AS", "9H", "TH", "JH", "QH", "KH", "AH", "9D", "TD", "JD", "QD", "KD", "AD", "9C", "TC", "JC", "QC", "KC", "AC", "8S", "8H", "8D", "8C", "BJ", "RJ"};
+
+    public Game(Team teamOne, Team teamTwo){
+        Game.teamOne = teamOne;
+        Game.teamTwo = teamTwo;
+    }
+
     /*
     Contains all the cards, ordered by set, with the player
     field of the respective card denoting who has the card.
@@ -11,120 +20,29 @@ public class Game {
     High Spades, High Hearts, High Diamonds, High Clubs,
     Eights/jokers
      */
+    static CardHolder[][] cards = new CardHolder[9][6];
+
+    static Team teamOne;
+    static Team teamTwo;
 
     // Converts the integer representation of a card
     // into the appropriate two-character format
     public static String cardIntToChar(int card){
-        int setOfCard = card / 6;
-        int cardNum = card % 6;
-        String cardChars = "";
-        if(setOfCard != 8){
-            // not part of eights set
-            if(setOfCard < 4){
-                cardChars += cardNum + 2;
-            } else {
-                switch (cardNum) {
-                    case 0 -> cardChars += '9';
-                    case 1 -> cardChars += 'T';
-                    case 2 -> cardChars += 'J';
-                    case 3 -> cardChars += 'Q';
-                    case 4 -> cardChars += 'K';
-                    case 5 -> cardChars += 'A';
-                }
-            }
-
-            switch (setOfCard % 4) {
-                case 0 -> cardChars += 'S';
-                case 1 -> cardChars += 'H';
-                case 2 -> cardChars += 'D';
-                case 3 -> cardChars += 'C';
-            }
-        } else {
-            if(cardNum > 3){
-                if(cardNum == 5){
-                    cardChars = "RJ";
-                } else {
-                    cardChars = "BJ";
-                }
-            } else {
-                cardChars += '8';
-                switch (cardNum) {
-                    case 0 -> cardChars += 'S';
-                    case 1 -> cardChars += 'H';
-                    case 2 -> cardChars += 'D';
-                    case 3 -> cardChars += 'C';
-                }
-            }
+        if(card < 0 || card > 54){
+            throw new IllegalArgumentException("ERROR: INVALID CARD");
         }
-        return cardChars;
+        return cardStrings[card];
     }
 
     // Converts the two-character format of a card
     // into the appropriate integer representation
     public static int cardCharToInt(String card){
-        if(card.length() != 2){
-            throw new IllegalArgumentException();
-        }
-        int set = -1;
-        int cardNum = -1;
-        boolean low = false;
-        try {
-            int cardValue = Integer.parseInt(card.substring(0, 1));
-            if(cardValue < 8){
-                low = true;
-                cardNum = cardValue - 2;
-            } else {
-                if(cardValue == 8){
-                    set = 8;
-                }else{
-                    cardNum = 0;
-                }
-            }
-        }catch (Exception e){
-            switch (card.charAt(0)){
-                case 'T':
-                    cardNum = 1;
-                break;
-                case 'J':
-                    cardNum = 2;
-                break;
-                case 'Q':
-                    cardNum = 3;
-                break;
-                case 'K':
-                    cardNum = 4;
-                break;
-                case 'A':
-                    cardNum = 5;
-                break;
-                case 'B':
-                    return 52;
-                case 'R':
-                    return 53;
+        for (int i = 0; i < cardStrings.length; i++) {
+            if(cardStrings[i].toLowerCase(Locale.ROOT).equals(card.toLowerCase(Locale.ROOT))){
+                return i;
             }
         }
-        if(set == -1){
-            int highSet = low ? 0 : 1;
-            set = switch (card.charAt(1)) {
-                case 'S' -> 4 * highSet;
-                case 'H' -> 1 + 4 * highSet;
-                case 'D' -> 2 + 4 * highSet;
-                case 'C' -> 3 + 4 * highSet;
-                default -> set;
-            };
-        } else {
-            switch (card.charAt(1)){
-                case 'S':
-                    return 48;
-                case 'H':
-                    return 49;
-                case 'D':
-                    return 50;
-                case 'C':
-                    return 51;
-            }
-        }
-        return set*6 + cardNum;
+        throw new IllegalArgumentException("ERROR: INVALID CARD");
     }
 
     // Returns the number of cards the player has
@@ -138,5 +56,61 @@ public class Game {
             }
         }
         return output;
+    }
+
+    // returns a player on the opposite team than that
+    // of activePlayer
+    public Player chooseOpponent(Player activePlayer){
+        System.out.println("Choose one of the following:");
+        for (int i = 0; i < teamOne.players.size(); i++) {
+            System.out.println(i + ") " + teamOne.players.get(i).NAME);
+        }
+        int inputtedInt = -1;
+        while(inputtedInt < 1 || inputtedInt > teamOne.players.size()){
+            while (Driver.input.hasNext()) {
+                if (Driver.input.hasNextInt()) {
+                    inputtedInt = Driver.input.nextInt();
+                    break;
+                } else {
+                    System.out.println("Please enter one of the numbers specified above!");
+                    Driver.input.next();
+                }
+            }
+        }
+        return teamOne.players.get(inputtedInt - 1);
+    }
+
+    /*
+    Repeatedly asks the player for a valid string input
+    and converts it into a card. Optional argument: array
+    of cards which are not valid choices to select.
+     */
+    public int chooseCardExcept(int[] excludedCards){
+        List<Integer> options = new ArrayList<>();
+        for (int i = 0; i < 54; i++) {
+            options.add(i);
+        }
+        for (int excluded:excludedCards) {
+            options.remove(excluded);
+        }
+        int[] optionsArray = new int[options.size()];
+        for (int i = 0; i < optionsArray.length; i++) {
+            optionsArray[i] = options.get(i);
+        }
+        return chooseCardFrom(optionsArray);
+    }
+
+    public int chooseCardFrom(int[] options){
+        int output;
+        while(true){
+            output = Driver.input.nextInt();
+            for (int option: options) {
+                if(option == output){
+                    return option;
+                } else {
+                    System.out.println("NOT A VALID CHOICE! TRY AGAIN...");
+                }
+            }
+        }
     }
 }
